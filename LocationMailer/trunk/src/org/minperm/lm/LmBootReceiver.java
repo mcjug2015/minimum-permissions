74c30fee5ba9b250a6851e672936a8e51dfd565b
victor.semenov@gmail.com <victor.semenov@gmail.com>
2011-01-25 01:52:42 +0000

    dealing with the "no connection at update time" issue

--- LocationMailer/trunk/src/org/minperm/lm/LmBootReceiver.java
+++ LocationMailer/trunk/src/org/minperm/lm/LmBootReceiver.java
@@ -9,6 +9,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 
 /**
  * Simple receiver that will handle the boot completed intent and send the
@@ -19,28 +20,37 @@ import android.content.Intent;
  */
 public class LmBootReceiver extends BroadcastReceiver {
 	@Override
-	public void onReceive(final Context context, final Intent bootintent) {
+	public void onReceive(final Context context, final Intent intent) {
+		if (intent.getAction().equals(
+				android.net.ConnectivityManager.CONNECTIVITY_ACTION)) {
+			boolean noConnection = intent.getBooleanExtra(
+					ConnectivityManager.EXTRA_NO_CONNECTIVITY, true);
+			if (!noConnection) {
+				SettingsDao.getInstance().setContext(context);
+				if (LmContainer.getInstance().getLmStatus().isSendingUpdates()
+						&& LmContainer.getInstance().getLmStatus()
+								.isFailedLastUpdate()) {
+					Intent sendOnce = new Intent(context, LmUpdateService.class);
+					context.startService(sendOnce);
+				}
+			}
+		} else {
+			doBootWork(context);
+		}
+	}
+
+	private void doBootWork(Context context) {
 		SettingsDao.getInstance().setContext(context);
 		if (LmContainer.getInstance().getLmStatus().isSendingUpdates()) {
 			PendingIntent mAlarmSender = PendingIntent.getService(context, 0,
 					new Intent(context, LmUpdateService.class), 0);
-			long firstTime = LmContainer.getInstance().getLmStatus()
-					.getLastUpdateDate()
-					+ LmContainer.getInstance().getUpdateInterval();
-
 			// Schedule the alarm!
 			AlarmManager am = (AlarmManager) context
 					.getSystemService(Activity.ALARM_SERVICE);
 			am
-					.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-							firstTime, LmContainer.getInstance()
-									.getUpdateInterval(), mAlarmSender);
-			
-			if (LmContainer.getInstance().getLmStatus().isFailedLastUpdate()) {
-				Intent mServiceIntent = new Intent();
-				mServiceIntent.setAction("org.minperm.lm.LmUpdateService");
-				context.startService(mServiceIntent);
-			}
+					.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, 0,
+							LmContainer.getInstance().getUpdateInterval(),
+							mAlarmSender);
 		}
 	}
 }
